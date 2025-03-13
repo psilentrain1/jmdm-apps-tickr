62a50121292cf35a1ccf7c38174ed98a9ce2c347
James Drake <info@jamesmdrakemedia.com>
2025-03-12 23:54:37 -0400

    feat: scaffolding ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
+import { Outlet } from "react-router";
+
 export default function App() {
-  return (
-    <>
-      <h1 className="text-4xl font-bold">Hello Electron</h1>
-      <p>Just testing the installation.</p>
-    </>
-  );
+  return <div></div>;
 }
