b8374042c39300172dfc0d2b90dd3d23fc09e437
James Drake <info@jamesmdrakemedia.com>
2025-03-13 14:45:47 -0400

    feat: colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
 import { MdMenuOpen } from "react-icons/md";
+import { SearchBar } from "./SearchBar";
 
 export function Header() {
   return (
-    <header className="flex h-11 flex-row border-1 border-pink-500 px-4">
+    <header className="flex h-11 flex-row bg-gray-900 px-4">
       <div className="flex flex-row">
-        <div className="border-1 border-orange-500">
+        <div className="flex flex-row items-center justify-center text-3xl">
           <MdMenuOpen />
         </div>
-        <div className="border-1 border-green-500">Tickr</div>
+        <div className="flex flex-row items-center justify-center px-4 text-3xl font-bold">
+          Tickr
+        </div>
       </div>
-      <div className="flex flex-grow flex-row justify-center border-1 border-purple-500">
-        Search goes here
+      <div className="flex flex-grow flex-row justify-center">
+        <div className="border-1 border-purple-500">
+          <SearchBar />
+        </div>
       </div>
     </header>
   );
