98d4527f857bb25f98fcd9a6582a3254862a6a7d
James Drake <info@jamesmdrakemedia.com>
2025-03-14 18:49:09 -0400

    feat: initial api work

---
+++ src/utils/api.utils.ts
@@ -0,0 +1,21 @@
+import { Ticker } from "../types/api.types";
+const api_key = process.env.API_KEY;
+
+// Get Ticker Info
+export function getAPITickerInfo(ticker: string) {
+  const url = `https://api.marketstack.com/v2/tickerinfo?access_key=${api_key}&ticker=${ticker}`;
+  const response = fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.data && data.data.length > 0) {
+        return data.data[0];
+      } else {
+        throw new Error("No data found for the given ticker");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching ticker info:", error);
+      //   return null;
+    });
+  return response;
+}
--- src/utils/db.utils.ts
+++ src/utils/db.utils.ts
@@ -1,13 +1,17 @@
 import { app } from "electron";
-import { dbLoc } from "../index";
 import fs from "fs";
 import path from "path";
 import sqlite from "better-sqlite3";
+import { Ticker } from "../types/api.types";
+import { dbLoc } from "../index";
+import { getAPITickerInfo } from "./api.utils";
+
+// Create the database connection
+const db = new sqlite(dbLoc);
 
 export function createDB() {
   fs.mkdirSync(path.join(app.getPath("appData"), "tickr"), { recursive: true });
 
-  const db = new sqlite(dbLoc);
   db.pragma("journal_mode = WAL");
 
   const query1 = `CREATE TABLE db (
@@ -17,11 +21,11 @@ export function createDB() {
 
   const query2 = `INSERT INTO db (db_key, db_value) VALUES ('db_version', '0.1');`;
 
-  const query3 = `CREATE TABLE symbol (
-    symbol_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    symbol TEXT NOT NULL,
-    symbol_type TEXT,
-    symbol_name TEXT,
+  const query3 = `CREATE TABLE ticker (
+    ticker_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ticker TEXT NOT NULL,
+    ticker_type TEXT,
+    ticker_name TEXT,
     industry TEXT,
     sector TEXT,
     description TEXT,
@@ -45,3 +49,34 @@ export function createDB() {
 
 // TODO: updateDB()
 // Maybe check against DB version and check tables
+
+// Get Ticker Info
+export function getTickerInfo(symbol: string) {
+  const query = `SELECT * FROM symbol WHERE symbol = ${symbol};`;
+  const result = db.prepare(query).all();
+
+  if (result.length === 0) {
+    // Get the ticker info from the API
+    getAPITickerInfo(symbol);
+  } else {
+    // Return the ticker info from the DB
+  }
+}
+
+// Add Ticker Info
+export function addTickerInfo(tickerInfo: Ticker) {
+  const query = `INSERT INTO ticker (ticker, ticker_type, ticker_name, industry, sector, description, cached_date) VALUES (?, ?, ?, ?, ?, ?, ?);`;
+  const result = db
+    .prepare(query)
+    .run(
+      tickerInfo.ticker,
+      tickerInfo.ticker_type,
+      tickerInfo.ticker_name,
+      tickerInfo.industry,
+      tickerInfo.sector,
+      tickerInfo.description,
+      new Date().toISOString(),
+    );
+
+  return result;
+}
