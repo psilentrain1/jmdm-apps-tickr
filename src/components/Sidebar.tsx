dd4d002ca835773344da3b06aa10b371b2b8be16
James Drake <info@jamesmdrakemedia.com>
2025-03-16 14:15:50 -0400

    feat: moved settings tab to bottom of sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,7 +16,7 @@ function Navigation() {
     "flex flex-row item-center box-border border-l-4 border-blue-500 px-4 py-3 font-medium text-gray-300 transition-colors duration-200 hover:bg-gray-700 hover:text-blue-300 active:text-blue-500";
   const iconClass = "flex flex-row item-center justify-center pr-2 text-2xl";
   return (
-    <nav>
+    <nav className="flex h-full flex-col justify-between">
       <ul className="">
         <NavLink to="/">
           {({ isActive }) => (
@@ -38,6 +38,8 @@ function Navigation() {
             </li>
           )}
         </NavLink>
+      </ul>
+      <ul>
         <NavLink to="/settings">
           {({ isActive }) => (
             <li className={isActive ? liActiveClass : liClass}>
