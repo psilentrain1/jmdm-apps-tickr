721b96fd5a0d8d8bb138e75bbc20f63ed9055a44
James Drake <info@jamesmdrakemedia.com>
2025-03-17 16:18:07 -0400

    feat: displaying chart

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,6 +1,31 @@
 import { useEffect } from "react";
 import { useStore } from "../hooks/useStore";
 import { DateRange } from "../types/component.types";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+);
+
+ChartJS.defaults.color = "oklch(0.967 0.003 264.542)";
 
 export function TickerChart({ ticker }: { ticker: string }) {
   const tickerChartTimeRange = useStore((state) => state.tickerChartTimeRange);
@@ -21,9 +46,55 @@ export function TickerChart({ ticker }: { ticker: string }) {
     getPrices();
   }, []);
 
-  useEffect(() => {
-    console.log(tickerChartData);
-  }, [tickerChartData]);
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+        // position: "top" as const,
+      },
+      title: {
+        display: false,
+        // text: "Chart.js Line Chart",
+      },
+      tooltip: {
+        callbacks: {
+          afterTitle: function (context) {
+            return `${context[0].formattedValue}`;
+          },
+          label: function () {
+            return "";
+          },
+          afterBody: function (context) {
+            return `Open: ${tickerChartData[context[0].dataIndex].open}
+High: ${tickerChartData[context[0].dataIndex].high}
+Low: ${tickerChartData[context[0].dataIndex].low}
+Close: ${tickerChartData[context[0].dataIndex].close}
+Volume: ${tickerChartData[context[0].dataIndex].volume}`;
+          },
+        },
+      },
+    },
+  };
+
+  const labels: string[] = tickerChartData.map((item) => {
+    const date = new Date(item.date);
+    return `${date.getMonth() + 1}/${date.getDate() + 1}/${date.getFullYear()}`;
+  });
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        fill: true,
+        label: "Close",
+        data: tickerChartData.map((item) => item.close),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        pointStyle: false,
+      },
+    ],
+  };
 
   return (
     <div>
@@ -44,7 +115,7 @@ export function TickerChart({ ticker }: { ticker: string }) {
           <option value="1y">1 Year</option>
         </select>
       </div>
-      <canvas id="tickerChart"></canvas>
+      <Line options={options} data={data} />
     </div>
   );
 }
