c8c88d36bce912ccdffc228b3b13c4be07cbfd33
James Drake <info@jamesmdrakemedia.com>
2025-03-17 22:17:29 -0400

    feat: add textual data to ticker page

--- src/views/Symbol.tsx
+++ src/views/Symbol.tsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
 import { useParams } from "react-router";
 import { TickerChart } from "../components/Charts";
+import { useStore } from "../hooks/useStore";
 
 export function Ticker() {
   const { ticker } = useParams();
+  const tickerInfo = useStore((state) => state.tickerInfo);
+  const setTickerInfo = useStore((state) => state.setTickerInfo);
 
   async function getTickerInfo() {
-    const tickerInfo = await window.api?.getTickerInfo(ticker);
-    console.log(tickerInfo);
+    setTickerInfo(await window.api?.getTickerInfo(ticker));
   }
 
   useEffect(() => {
@@ -16,9 +18,42 @@ export function Ticker() {
 
   return (
     <>
-      <h1>Symbol page</h1>
-      <p>{ticker}</p>
-      <TickerChart ticker={ticker} />
+      <div className="flex flex-row justify-between pb-2">
+        <div>
+          <h1 className="text-2xl font-bold">
+            {tickerInfo[0].ticker_name}
+            {" ("}
+            {tickerInfo[0].ticker}
+            {")"}
+          </h1>
+          <div className="text-gray-400">
+            {tickerInfo[0].sector}
+            {" - "}
+            {tickerInfo[0].industry}
+          </div>
+        </div>
+
+        <div>
+          <div className="text-2xl font-bold">
+            {"$"}
+            {tickerInfo[2].close}
+          </div>
+          <div>
+            <span>points</span>
+            <span>percentage</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between pb-4">
+        <span>Open: ${tickerInfo[2].open}</span>
+        <span>High: ${tickerInfo[2].high}</span>
+        <span>Low: ${tickerInfo[2].low}</span>
+        <span>Close: ${tickerInfo[2].close}</span>
+        <span>Volume: {tickerInfo[2].volume}</span>
+      </div>
+      <div>
+        <TickerChart ticker={ticker} />
+      </div>
     </>
   );
 }
