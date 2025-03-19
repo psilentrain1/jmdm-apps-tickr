0c683efb2f63b36e915128a35ce1484bf37e70b2
James Drake <info@jamesmdrakemedia.com>
2025-03-19 10:32:38 -0400

    feat: implemented and styled toasts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
@@ -11,6 +12,15 @@ export default function App() {
         <Sidebar />
         <main className="mt-11 mb-9 ml-48 flex-grow overflow-auto overscroll-contain bg-gray-800 p-4">
           <Outlet />
+          <Toaster
+            position="bottom-right"
+            toastOptions={{
+              style: {
+                background: "rgb(55, 65, 81)",
+                color: "rgb(243, 244, 246)",
+              },
+            }}
+          />
         </main>
       </div>
       <Footer />
