a27db9e710a6c323736a2c7616a55d1b681f4396
James Drake <info@jamesmdrakemedia.com>
2025-03-22 15:17:58 -0400

    feat: settings page UI

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -1,7 +1,19 @@
 export function Settings() {
   return (
-    <>
-      <h1>Settings go here</h1>
-    </>
+    <div className="mx-auto max-w-3xl">
+      <h1 className="pb-2 text-3xl font-bold">Settings</h1>
+      <div className="">
+        <div className="flex flex-row items-center justify-between border-y-1 border-gray-100 px-4 py-2">
+          <span>Default date range for charts:</span>
+          <select name="dateRange" id="dateRange">
+            <option value="5d">5 days</option>
+            <option value="1m">1 month</option>
+            <option value="3m">3 months</option>
+            <option value="6m">6 months</option>
+            <option value="1y">1 year</option>
+          </select>
+        </div>
+      </div>
+    </div>
   );
 }
