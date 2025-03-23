197e801a5729fd8edecbf4a118bfcc4c81611256
James Drake <info@jamesmdrakemedia.com>
2025-03-23 18:37:21 -0400

    fix: removed call to API that caused error

--- src/utils/db.utils.ts
+++ src/utils/db.utils.ts
@@ -10,7 +10,7 @@ import {
   Watchlist,
 } from "../types/api.types";
 import { dbLoc, db } from "../index";
-import { getAPITickerInfo } from "./api.utils";
+// import { getAPITickerInfo } from "./api.utils";
 import { DateRange } from "../types/component.types";
 
 export function createDB() {
@@ -78,11 +78,11 @@ export function getTickerInfo(symbol: string): TickerInfo | null {
   const ticker = stmtTicker.get(symbol) as Ticker | undefined;
   const lastClose = stmtLastClose.all(symbol) as Prices[];
 
-  if (!ticker || lastClose.length < 2) {
+  /*  if (!ticker || lastClose.length < 2) {
     // If not found, fetch from API or return null
     getAPITickerInfo(symbol);
     return null;
-  }
+  } */
   const a = lastClose[1].close;
   const b = lastClose[0].close;
   let gain: boolean;
