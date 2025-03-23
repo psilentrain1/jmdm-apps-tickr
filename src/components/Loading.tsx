af94a8a5ed39e7a8984f262e14cc8dcff41c35f1
James Drake <info@jamesmdrakemedia.com>
2025-03-23 16:52:55 -0400

    feat: implemented loading bar when data is loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,9 @@
-import { BarLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners";
 
 export function Loading() {
   return (
     <div className="flex h-full items-center justify-center">
-      <BarLoader color="rgba(243, 244, 246, 1)" />
+      <ClipLoader size={70} color="rgba(243, 244, 246, 1)" />
     </div>
   );
 }
