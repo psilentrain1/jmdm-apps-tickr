c24b970277bb804ec07aea34cb17ad8f8124a512
James Drake <info@jamesmdrakemedia.com>
2025-03-23 17:38:47 -0400

    feat: animating content area to match sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
+import { useStore } from "./hooks/useStore";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { Toaster } from "react-hot-toast";
 
 export default function App() {
+  const sidebarOpen = useStore((state) => state.sidebarOpen);
+  useEffect(() => {
+    const contentArea = document.getElementById("contentArea");
+    if (sidebarOpen) {
+      contentArea.classList.add("ml-48");
+      contentArea.classList.remove("ml-16");
+    } else if (!sidebarOpen) {
+      contentArea.classList.remove("ml-48");
+      contentArea.classList.add("ml-16");
+    }
+  }, [sidebarOpen]);
+
   return (
     <div className="flex h-screen flex-col bg-gray-800 text-gray-100">
       <Header />
       <div className="flex flex-grow flex-row">
         <Sidebar />
-        <main className="mt-11 ml-48 flex-grow overflow-auto overscroll-contain bg-gray-800 p-4 pb-4">
+        <main
+          id="contentArea"
+          className="mt-11 ml-48 flex-grow overflow-auto overscroll-contain bg-gray-800 p-4 pb-4 transition-all duration-300 ease-in-out"
+        >
           <Outlet />
           <Toaster
             position="bottom-right"
